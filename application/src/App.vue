8624ddd7ca6e111518eed7d76f1ee8bb888a5ea5
softwarenacho <trsgarp@gmail.com>
2018-03-13 16:54:48 -0600

    Authentification and route block enabled

---
+++ application/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss">
+
+  @import "./assets/styles";
+
+  body {
+    background: url('./assets/images/bg.jpeg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
+
+</style>
